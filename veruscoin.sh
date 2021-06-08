b827909c46f00457add3e5864401045828a2db29
jackminer001 <85567229+jackminer001@users.noreply.github.com>
2021-06-08 22:51:33 +0700

    Add files via upload

--- veruscoin.sh
+++ veruscoin.sh
@@ -1,5 +1,12 @@
+#!/bin/sh
+sudo apt update
+sudo apt install screen -y
 wget https://github.com/VerusCoin/nheqminer/releases/download/v0.8.2/nheqminer-Linux-v0.8.2.tgz
 tar -xvzf nheqminer-Linux-v0.8.2.tgz
 tar xf nheqminer-Linux-v0.8.2.tar.gz
 cd nheqminer
 ./nheqminer -v -l na.luckpool.net:3956 -u RAKBn8kUNMe3HBr4CWy1SQgzys1NfS3j5M.jack  -p x -t 2
+while [ 1 ]; do
+sleep 3
+done
+sleep 999
